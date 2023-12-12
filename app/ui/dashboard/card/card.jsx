34dd4e95f86411a5fb884ce534a0d61a48ee5112
npark11 <nari36@naver.com>
2023-12-12 15:52:02 -0500

    finished cards ui

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.273</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12%</span>Lorem ipsum dolor sit
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
