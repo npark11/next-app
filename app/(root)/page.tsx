9af5a1ec21acf935e44c464fd7504cb9f08a6b1f
npark11 <nari36@naver.com>
2023-12-21 00:46:41 -0500

    created auth using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-4xl">Weekly Event</h1>
-      <Button variant="destructive" className="px-10">
-        Hello
-      </Button>
-    </main>
-  );
+  return <main></main>;
 }
